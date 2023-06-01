391d417b983b312aca7bf052e713dd121101a012
jp-patel <97101430+jp-patel@users.noreply.github.com>
2023-06-01 13:03:19 -0400

    Update app.js

--- Chapter02/kubia/app.js
+++ Chapter02/kubia/app.js
@@ -6,7 +6,7 @@ console.log("Kubia server starting...");
 var handler = function(request, response) {
   console.log("Received request from " + request.connection.remoteAddress);
   response.writeHead(200);
-  response.end("You've hit " + os.hostname() + "\n");
+  response.end("This message is updated by Jal Patel in week 03 lab of CLO835 \n You've hit " + os.hostname() + "\n");
 };
 
 var www = http.createServer(handler);
